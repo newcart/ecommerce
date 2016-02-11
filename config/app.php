cbe65045712ab58264629deeaf05003902dea311
kallef <prhost@prhost.com.br>
2016-02-11 19:48:58 -0200

    fix gitignore

--- config/app.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-//define o nome da pasta do admin
-define('PATH_ADMIN', 'admin');
--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// DB
-define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'open_commerce');
-define('DB_PREFIX', 'oc_');
