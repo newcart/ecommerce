cd56ab9571b56b94f5634d38ac8fe4c42ce56ca8
kallef <prhost@prhost.com.br>
2016-02-26 23:09:51 -0300

    fix route panel and others fix

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
 <?php
-
-define('DIR_ROOT', realpath(__DIR__ . '/'));
+define('DIR_ROOT', realpath(__DIR__));
 
 //Composer autoload
 require_once(DIR_ROOT . '/vendor/autoload.php');
 
 // Load
 require_once(DIR_ROOT . '/vendor/newcart/system/src/Newcart/System/load.php');
+
+//check access admin panel
+parse_str($_SERVER['QUERY_STRING'], $output);
+$route = isset($output['_route_']) ? $output['_route_'] : '';
+
+//Set environment
+if (substr($route, 0, strlen($config->get('admin_path'))) == $config->get('admin_path')) {
+    $config->set('environment', 'admin');
+} else {
+    $config->set('environment', 'catalog');
+}
+
+//Constants
+require_once(DIR_ROOT . '/vendor/newcart/system/src/Newcart/System/constants.php');
+
+//Bootstrap
+require_once(DIR_APPLICATION . 'bootstrap.php');
 
 // Dispatch
 $controller->dispatch($action, new Action('error/not_found'));
